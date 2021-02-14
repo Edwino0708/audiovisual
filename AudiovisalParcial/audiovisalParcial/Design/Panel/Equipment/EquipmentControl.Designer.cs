c0d06ea7ae9329624532f9e05dda17c03edc95e3
Viictor06 <49105504+Viictor06@users.noreply.github.com>
2021-02-13 23:34:36 -0400

    fix error

--- AudiovisalParcial/audiovisalParcial/Design/Panel/Equipment/EquipmentControl.Designer.cs
+++ AudiovisalParcial/audiovisalParcial/Design/Panel/Equipment/EquipmentControl.Designer.cs
@@ -29,8 +29,16 @@ namespace audiovisalParcial.Design.Panel.Equipment
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // EquipmentControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "EquipmentControl1";
+            this.Size = new System.Drawing.Size(370, 310);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
