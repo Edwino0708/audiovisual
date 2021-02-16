9274130120f883050236d945ecdcb37e82cdb0dc
Edwin Tejada <edwintejadar@gmail.com>
2021-02-16 13:29:09 -0400

    msater esta bien

--- AudiovisalParcial/audiovisalParcial/Design/Menu/Maintances/MaintenanceMenuControl.cs
+++ AudiovisalParcial/audiovisalParcial/Design/Menu/Maintances/MaintenanceMenuControl.cs
@@ -1,10 +1,7 @@
 ﻿using audiovisalParcial.Design.Panel.EquipmentType;
-<<<<<<< HEAD
-using audiovisalParcial.Design.Panel.Users;
-=======
 using audiovisalParcial.Design.Panel.Maintenances.Brands;
 using audiovisalParcial.Design.Panel.Models;
->>>>>>> origin/jm_developer
+using audiovisalParcial.Design.Panel.Users;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,26 +18,19 @@ namespace audiovisalParcial.Design.Menu.Maintances
     {
         private Form1 form;
         private EquipmentTypeControl equimentType;
-<<<<<<< HEAD
         private UsersControl user;
-=======
         private BrandControl brand;
         private ModelsControl model;
 
->>>>>>> origin/jm_developer
 
         public MaintenanceMenuControl(Form1 form)
         {
             InitializeComponent();
             this.form = form; 
             equimentType = new EquipmentTypeControl();
-<<<<<<< HEAD
             user = new UsersControl();
-
-=======
             brand = new BrandControl();
             model = new ModelsControl();
->>>>>>> origin/jm_developer
         }
 
 
