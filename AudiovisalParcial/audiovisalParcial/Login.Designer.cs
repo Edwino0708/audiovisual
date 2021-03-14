f227cd2b9e175c4556410a6fbc18cb57c9bd73ac
Junior Mena <juniormelpelotero@hotmail.com>
2021-03-13 21:29:36 -0400

    Login

---
+++ AudiovisalParcial/audiovisalParcial/Login.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace audiovisalParcial
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdLogin = new System.Windows.Forms.Button();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdLogin
+            // 
+            this.cmdLogin.Location = new System.Drawing.Point(15, 104);
+            this.cmdLogin.Name = "cmdLogin";
+            this.cmdLogin.Size = new System.Drawing.Size(319, 25);
+            this.cmdLogin.TabIndex = 9;
+            this.cmdLogin.Text = "&Iniciar Sesión";
+            this.cmdLogin.UseVisualStyleBackColor = true;
+            this.cmdLogin.Click += new System.EventHandler(this.cmdLogin_Click);
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(67, 59);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(267, 20);
+            this.txtClave.TabIndex = 8;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(67, 12);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(267, 20);
+            this.txtUsuario.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Clave";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Usuario";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 137);
+            this.Controls.Add(this.cmdLogin);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdLogin;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ AudiovisalParcial/audiovisalParcial/Login.cs
@@ -0,0 +1,46 @@
+﻿using audiovisalParcial.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace audiovisalParcial
+{
+    public partial class Login : Form
+    {
+        private AudiovisualDbEntities audiovisualEntities = new AudiovisualDbEntities();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdLogin_Click(object sender, EventArgs e)
+        {
+            User usuario = (from u in audiovisualEntities.Users
+                               where u.FirstName.Equals(txtUsuario.Text) &&
+                                     u.IdentificationCard.Equals(txtClave.Text)
+                               select u).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                MessageBox.Show("Credenciales incorrectas");
+            }
+            else if (usuario.Enabled.Equals("0"))
+            {
+                MessageBox.Show("Credenciales incorrectas");
+            }
+            else
+            {
+                MessageBox.Show("Bienvenido " + txtUsuario.Text);
+                Form1 frmMenu = new Form1();
+                this.Hide();
+                frmMenu.ShowDialog();
+            }
+        }
+    }
+}
